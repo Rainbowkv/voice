ce1fe8b977f24109a1a9fe786d30249c6e95e0b0
Rainbowkv <981512226@qq.com>
2025-03-19 21:20:49 +0800

    汉化

--- qb-radio/locales/en.lua
+++ qb-radio/locales/en.lua
@@ -1,13 +1,13 @@
 local Translations ={
-    ["not_on_radio"] = "You're not connected to a signal",
-    ["joined_to_radio"] = "You're connected to: %{channel}",
-    ["restricted_channel_error"] = "You can not connect to this signal!",
-    ["invalid_radio"] = "This frequency is not available.",
-    ["you_on_radio"] = "You're already connected to this channel",
-    ["you_leave"] = "You left the channel.",
-    ['volume_radio'] = 'New volume %{value}',
-    ['decrease_radio_volume'] = 'The radio is already set to maximum volume',
-    ['increase_radio_volume'] = 'The radio is already set to the lowest volume',
+    ["not_on_radio"] = "您没有在任何无线电频道",
+    ["joined_to_radio"] = "连接至频道: %{channel}",
+    ["restricted_channel_error"] = "您没有权限连接到公职频道!",
+    ["invalid_radio"] = "此无线电频率不能使用",
+    ["you_on_radio"] = "您已经连接到了此频道",
+    ["you_leave"] = "您离开了无线电频道",
+    ['volume_radio'] = '新的音量： %{value}',
+    ['decrease_radio_volume'] = '已经是最大音量',
+    ['increase_radio_volume'] = '已经是最低音量',
 }
 
 Lang = Lang or Locale:new({
